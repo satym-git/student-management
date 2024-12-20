39406c0d21909c73f2b3bc715f01dd55d994ee74
Satyam <Satyam@192.168.1.47>
2024-12-20 10:37:04 -0800

    final setup

--- studentManagement/src/main/java/com/example/studentManagement/StudentManagementApplication.java
+++ studentManagement/src/main/java/com/example/studentManagement/StudentManagementApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StudentManagementApplication {
     public static void main(String[] args) {
+    	  System.setProperty("spring.profiles.active", "dev");
         SpringApplication.run(StudentManagementApplication.class, args);
     }
 }
